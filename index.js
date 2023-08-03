b1b7abff16ba12c4efd007890a71e136064d7368
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-08-03 10:52:45 +0600

    Create api for get by id

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
+    // get by id
+    app.get('/alltask/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    })
     // post api created
     app.post('/addtask', async(req, res) =>{
         const newTask = req.body;
